cd8b9a8c0ba603b637bc857c19d05cd19793bfd1
Junhyeok Yun <claudead777@gmail.com>
2021-02-07 19:41:06 +0900

    Update search screen

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 function clearInput() {
-    let clean_arr = document.getElementsByClassName('search-box__window');
+    let clean_arr = document.getElementsByClassName("search-box__window");
     for (let i = 0; i < clean_arr.length; i++) {
         clean_arr[i].value = null;
     }
