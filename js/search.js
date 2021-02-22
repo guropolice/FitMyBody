2b35da6e7bee264415130b03ca09680afe85c3f0
Junhyeok Yun <claudead777@gmail.com>
2021-02-22 10:36:38 +0900

    Set up Firebase

--- js/search.js
+++ js/search.js
@@ -1,3 +1,5 @@
+import firebase from "./firebase";
+
 function clearInput() {
     let clean_arr = document.getElementsByClassName("search-box__window");
     for (let i = 0; i < clean_arr.length; i++) {
