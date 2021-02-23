bea110245f9e4ab945d2dcfd0035ef3514ed09cc
Junhyeok Yun <claudead777@gmail.com>
2021-02-23 23:09:37 +0900

    Fix Firebase

--- js/myFirebase.js
+++ js/myFirebase.js
@@ -7,22 +7,22 @@ const firebaseConfig = {
     storageBucket: "findyourfit-f796c.appspot.com",
     messagingSenderId: "896675566116",
     appId: "1:896675566116:web:1834adc1d309fd56ee7eb1"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 
-export const authService = firebase.auth();
+// export const authService = firebase.auth();
 
-const login = document.querySelector(".login-form");
+// const login = document.querySelector(".login-form");
 
-let authProvider = new firebase.auth.GoogleAuthProvider();
+// let authProvider = new firebase.auth.GoogleAuthProvider();
 
-authService.onAuthStateChanged(function(user){
-    if(user){
-        //인증성공
-        console.log(user);
-    } else{
-        //인증실패
-        authService.signInWithPopup(authProvider);
-    }
-});
+// authService.onAuthStateChanged(function(user){
+//     if(user){
+//         //인증성공
+//         console.log(user);
+//     } else{
+//         //인증실패
+//         authService.signInWithPopup(authProvider);
+//     }
+// });
--- js/search.js
+++ js/search.js
@@ -1,5 +1,3 @@
-import firebase from "./firebase";
-
 function clearInput() {
     let clean_arr = document.getElementsByClassName("search-box__window");
     for (let i = 0; i < clean_arr.length; i++) {
