b3519a537b2ee1e1fbb0c518f394dfa4260d31a9
guroprotector <sokkk6018@naver.com>
2021-03-02 00:59:20 +0900

    가입하기 +로그인 구현

---
+++ .firebase/signUp_init.js
@@ -0,0 +1,26 @@
+const Config = {
+    apiKey: "AIzaSyCbLIIXGs2pZMq7PAQ6OQbOIwBWHOXy_uA",
+    authDomain: "findyourfit-f796c.firebaseapp.com",
+    databaseURL: "https://findyourfit-f796c-default-rtdb.firebaseio.com/",
+    storageBucket: "findyourfit-f796c.appspot.com"
+};
+
+firebase.initializeApp(Config);
+
+
+
+// Sign out
+function signOut() {
+    if(!confirm("Do you really want to log out?")) {
+        return;
+    }
+    
+    firebase.auth().signOut().then(() => {
+        location.replace('http://localhost:5000/');
+    }, function(e) {
+        lastWork = "authorized";
+        $("#error #errmsg").html(e.message)
+        $("#error").show();
+        $("#authorized").hide();
+    });
+}
