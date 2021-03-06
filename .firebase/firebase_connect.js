664fb4de9b256543c08056a6091c38c7c2e77719
KKGB <69628911+KKGB@users.noreply.github.com>
2021-03-06 19:58:44 +0900

    email message

---
+++ .firebase/firebase_connect.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCbLIIXGs2pZMq7PAQ6OQbOIwBWHOXy_uA",
+    authDomain: "findyourfit-f796c.firebaseapp.com",
+    databaseURL: "https://findyourfit-f796c-default-rtdb.firebaseio.com",
+    projectId: "findyourfit-f796c",
+    storageBucket: "findyourfit-f796c.appspot.com",
+    messagingSenderId: "896675566116",
+    appId: "1:896675566116:web:1834adc1d309fd56ee7eb1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- js/search_pwd.js
+++ js/search_pwd.js
@@ -1,59 +1,29 @@
-function searchPwd(){
-    const resetPassword = document.getElementById('resetPassword');
-    const mailField = document.getElementById('mail');
-
-
-    const auth = firebase.auth();
+$("#btn-resetPassword").click(function()
+{
+    var auth = firebase.auth();
+    var email = $("#fiyfi_id").val();
     
-    const resetPasswordFunction = () =>{
-        const email = mailField.value;
 
-        auth.sendPasswordResetEmail(email)
-        .then(()=>{
-            console.log('이메일이 성공적으로 보내졌습니다!');
-            alert('이메일을 확인하세요!');
+    if(email != "")
+    {
+        auth.sendPasswordResetEmail(String(fiyfi_id)).then(function()
+        {
+                window.alert("이메일을 확인해주세요!!");
         })
-        .catch(error => {
-            console.error(error);
-            alert('fail');
-        })
+        .catch(function(error)
+        {
+            var errorCode = error.code;
+            var errorMessage = error.message;
 
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            window.alert("잘못된 이메일입니다. 다시 확인해주세요!");
+        });
     }
 
-    resetPassword.addEventListener('click', resetPasswordFunction);
-
-
-
-    /*const t = document.getElementById('target');
-
-    t.addEventListener('submit', function (e) {
-        e.preventDefault();
-        let userEmail = t['email-seek'].value;
-*/
-
-       /* var auth = firebase.auth();
-        var emailAddress = $("fiyfi_id").val();
-
-        firebase.auth().sendPasswordResetEmail(emailAddress).then(function(){
-            alert('이메일을 확인하세요!');
-        }).catch(function(error){
-            alert('실패하셨어요!');
-        })*/
-
-
-
-
-
-
-        /*if(emailAddress !='') {
-            firebase.auth().sendPasswordResetEmail(emailAddress).then(function(){
-                console.log('비밀번호 reset || Email 전송');
-            }).catch(function(error) {
-                var errorMessage = error.message;
-                console.log('에러 메시지:'+errorMessage);
-            });
-        } else{
-            console.log('이메일이 맞지 않습니다.');
-            
-        }*/
-    };
+    else
+    {
+        window.alert("Please write your email first.");
+    }
+});
