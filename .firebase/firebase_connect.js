eef3320ad776bc236f61714563381dcff5ac1097
Junhyeok Yun <claudead777@gmail.com>
2021-03-07 17:41:17 +0900

    firebase

--- .firebase/firebase_connect.js
+++ .firebase/firebase_connect.js
@@ -1,11 +1,18 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
 var firebaseConfig = {
-    apiKey: "AIzaSyCbLIIXGs2pZMq7PAQ6OQbOIwBWHOXy_uA",
-    authDomain: "findyourfit-f796c.firebaseapp.com",
-    databaseURL: "https://findyourfit-f796c-default-rtdb.firebaseio.com",
-    projectId: "findyourfit-f796c",
-    storageBucket: "findyourfit-f796c.appspot.com",
-    messagingSenderId: "896675566116",
-    appId: "1:896675566116:web:1834adc1d309fd56ee7eb1"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCbLIIXGs2pZMq7PAQ6OQbOIwBWHOXy_uA",
+  authDomain: "findyourfit-f796c.firebaseapp.com",
+  databaseURL: "https://findyourfit-f796c-default-rtdb.firebaseio.com",
+  projectId: "findyourfit-f796c",
+  storageBucket: "findyourfit-f796c.appspot.com",
+  messagingSenderId: "896675566116",
+  appId: "1:896675566116:web:1834adc1d309fd56ee7eb1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
+export const authService = firebase.auth();
